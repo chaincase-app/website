ac5aae9b9efba78123fc0f80d425a167416eb7e3
Daniel Gould <dan@dannygould.com>
2016-09-14 12:07:13 -0400

    added head model

--- public/assets/phys.js
+++ public/assets/phys.js
@@ -3,7 +3,6 @@ var canvas,
    	w,
    	h,
    	world,
-   	boxBody, 
 	textBody,
 	floorBody, 
 	ceilingBody, 
@@ -12,8 +11,10 @@ var canvas,
    	mouseConstraint,
 	mouseBody;
 
-var scaleX = 1;
-var scaleY = -1;
+var boxBodies = [];
+var face = document.createElement('img');
+face.src = 'assets/img/dannygouldface.gif';
+face.alt = 'Danny Gould\'s face';
 init();
 animate();
 
@@ -30,15 +31,16 @@ function init() {
 	ctx.lineWidth = 1;
 
 	// Init p2.js
-	world = new p2.World({gravity: [0, -800]});
+	world = new p2.World({gravity: [0, 980]});
 
 	// Add walls
+	 /*
 	floorBody = new p2.Body({
 		position: [0, -h / 2]
 	});
 	floorBody.addShape(new p2.Plane);
 	world.addBody(floorBody);
-
+*/
 	ceilingBody = new p2.Body({
 		position: [0, h / 2],
 		angle: Math.PI
@@ -60,16 +62,7 @@ function init() {
 	world.addBody(leftWallBody);
 
 	// Add a box
-	boxShape = new p2.Box({ width: 20, height: 40 });
-	boxBody = new p2.Body({
-	  mass:1,
-	  position:[0,300],
-	  angularVelocity:1
-	});
-	boxBody.addShape(boxShape);
-	world.addBody(boxBody);
-
-
+	addBox([0, canvas.width/3]);
 	// Create a body for the cursor
 	mouseBody = new p2.Body();
 	world.addBody(mouseBody);
@@ -80,7 +73,7 @@ function init() {
 	  var position = getPhysicsCoord(event);
 
 	  // Check if the cursor is inside the box
-	  var hitBodies = world.hitTest(position, [boxBody]);
+	  var hitBodies = world.hitTest(position, boxBodies);
 
 	  if(hitBodies.length){
 
@@ -90,11 +83,13 @@ function init() {
 
 		// Create a RevoluteConstraint.
 		// This constraint lets the bodies rotate around a common point
-		mouseConstraint = new p2.RevoluteConstraint(mouseBody, boxBody, {
+		mouseConstraint = new p2.RevoluteConstraint(mouseBody, hitBodies[0], {
 		  worldPivot: position,
 		  collideConnected:false
 		});
 		world.addConstraint(mouseConstraint);
+	  } else {
+		  addBox([position[0], position[1]]);
 	  }
 	});
 
@@ -110,9 +105,44 @@ function init() {
 	canvas.addEventListener('mouseout', removeMouseConstraint, false);
 
 }
-	
+function addBox(position) {	
+	vertices = [
+		[48,9],
+		[33,45],
+		[17,68],
+	   	[0,66],
+	   	[-15, 60],
+		[-37, 35],
+		[-50, 10],
+		[-43, -18],
+		[-30, -38],
+		[0, -48],
+	   	[28,-44],
+	   	[48,-16]
+	]
+	//circleShape = new p2.Circle({radius: 100});
+	convexShape = new p2.Convex({vertices: vertices})
+	boxBodies.push(new p2.Body({
+		mass:1,
+		position:position,
+		angularVelocity:1
+	}));
+	boxBodies[boxBodies.length - 1].addShape(convexShape);
+	world.addBody(boxBodies[boxBodies.length - 1]);
+	/*
+	boxShape = new p2.Box({ width: 20, height: 40 });
+	boxBodies.push(new p2.Body({
+		mass:1,
+		position:position,
+		angularVelocity:1
+	}));	
+	boxBodies[boxBodies.length - 1].addShape(new p2.Box({width:20, height:40}));
+	world.addBody(boxBodies[boxBodies.length - 1]);
+	*/
+}
+
+
 function removeMouseConstraint(event) {
-	console.log(mouseConstraint);
 	world.removeConstraint(mouseConstraint);
 	mouseConstraint = null;
 }
@@ -120,25 +150,36 @@ function removeMouseConstraint(event) {
 // Convert a canvas coordiante to physics coordinate
 function getPhysicsCoord(mouseEvent){
   var rect = canvas.getBoundingClientRect();
+  //TODO
   var x = mouseEvent.clientX - rect.left;
   var y = mouseEvent.clientY - rect.top;
 
-  x = (x - w / 2) / scaleX;
-  y = (y - h / 2) / scaleY;
+  x = (x - w / 2);
+  y = (y - h / 2);
 
   return [x, y];
 }
 
 function drawbox(){
-	ctx.beginPath();
-	var x = boxBody.position[0],
-		y = boxBody.position[1];
-	ctx.save();
-	ctx.translate(x, y);        // Translate to the center of the box
-	ctx.rotate(boxBody.angle);  // Rotate to the box body frame
-	ctx.rect(-boxShape.width/2, -boxShape.height/2, boxShape.width, boxShape.height);
-	ctx.stroke();
-	ctx.restore();
+	for(i = 0; i < boxBodies.length; i++) {
+		ctx.beginPath();
+		var x = boxBodies[i].position[0],
+			y = boxBodies[i].position[1];
+		ctx.save();
+		ctx.translate(x, y);        // Translate to the center of the box
+		ctx.rotate(boxBodies[i].angle);  // Rotate to the box body frame
+		ctx.drawImage(face, -face.width/4, -face.height/4 + 10, face.width/2, face.height/2);
+		ctx.rotate(Math.PI);
+		/* draws vertices */
+		/*
+		for(j = 0; j < vertices.length; j++) {
+			ctx.lineTo(-vertices[j][0], -vertices[j][1]);
+		}	
+		*/
+		ctx.stroke();
+		ctx.restore();
+
+	}
 }
 
 function drawFloors(){
@@ -156,7 +197,6 @@ function render(){
 	// Transform the canvas
 	ctx.save();
 	ctx.translate(w/2, h/2); // Translate to the center
-	ctx.scale(scaleX, scaleY);
 
 	// Draw all bodies
 	drawbox();
