a67b9f8426604dae9599cc9439f5e4a577297b3c
DanGould <dan@dannygould.com>
2019-04-07 12:37:57 -0400

    render blog post previews on home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,29 @@
+require 'nokogiri'
+require 'ostruct'
+
 class StaticPagesController < ApplicationController
   def home
+    # get blog previews from all of the md posts
+
+    posts = Post.get_posts 
+    @blog_previews = []
+    posts.first(5).each do |post|
+      post = Post.new(post)
+      preview = OpenStruct.new
+      preview.content = get_preview(post)
+      preview.name = post.name
+      @blog_previews << preview
+    end
   end
+
+  private
+
+  def get_preview(post)
+    post = render_to_string partial: "blog/#{post.name}"
+    #whitespace .gsub(/\s+/, ""
+    doc = Nokogiri::HTML(post)
+    post = doc.xpath('//body/*[position() < 5]').to_html()
+    @post = render_to_string inline: post
+  end
+
 end
