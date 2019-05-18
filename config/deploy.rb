6861789c4d4f1e791049cd685426f4e056637939
DanGould <dan@dannygould.com>
2019-05-18 16:22:42 -0400

    no database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,6 @@ set :deploy_to, "/var/www/portfolio"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml'
 append :linked_files, 'config/master.key'
 
 # Default value for linked_dirs is []
