c03f58f463a7975eb439e6c2774adeaf7ebc4442
DanGould <d@ngould.dev>
2019-10-08 15:41:41 -0400

    clean users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,16 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to "/", notice: 'Updates coming your way!'
-      # no else, worst case, user already exists. Else, form should capture it
+      redirect_to root_path, notice: 'Updates coming your way!'
+    else
+      redirect_to root_path, notice: "Failed to save. If you're not already signed up, try again"
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
-
     # Only allow a trusted parameter "white list" through.
     def user_params
       params.require(:user).permit(:email)
